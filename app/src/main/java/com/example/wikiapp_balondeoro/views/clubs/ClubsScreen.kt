236191444302e18ec3d9563459757d883c3dc83c
darley523 <gac.av@hotmail.com>
2025-11-15 17:24:58 -0300

    pantalla de clubes

--- app/src/main/java/com/example/wikiapp_balondeoro/views/clubs/ClubsScreen.kt
+++ app/src/main/java/com/example/wikiapp_balondeoro/views/clubs/ClubsScreen.kt
@@ -1,11 +1,30 @@
 package com.example.wikiapp_balondeoro.views.clubs
 
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
-import androidx.lifecycle.viewmodel.compose.viewModel
-import androidx.navigation.NavHostController
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import com.example.wikiapp_balondeoro.clases.crearClubes
+import com.example.wikiapp_balondeoro.views.common.Barra
 
 @Composable
-fun ClubsScreen(viewModel: ClubsViewModel = viewModel(), navController: NavHostController) {
-    Text(text = "Clubs Screen")
+fun ClubsScreen(navController: NavController) {
+    val clubes = crearClubes()
+    LazyColumn(modifier = Modifier.fillMaxSize()) {
+        items(clubes) { club ->
+            Barra(
+                titulo = club.nombre
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true, heightDp = 1200)
+@Composable
+fun ClubsScreenPreview() {
+    ClubsScreen(navController = rememberNavController())
 }
