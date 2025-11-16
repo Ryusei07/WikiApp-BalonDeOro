2fd6037ea51d154d31829a7f34aa890822796d03
Ryusei07 <diegoignaciozamora@gmail.com>
2025-11-16 14:54:03 -0300

    Mejorar vista ClubsScreen.kt, crear atributo Imagen para Clubes en Club.kt y añadir la funcion de imagen de los clubes a Funciones.kt

--- app/src/main/java/com/example/wikiapp_balondeoro/clases/Club.kt
+++ app/src/main/java/com/example/wikiapp_balondeoro/clases/Club.kt
@@ -3,6 +3,8 @@ package com.example.wikiapp_balondeoro.clases
 data class Club (
     val nombre: String,
     val cantidadBalones: Int,
-    val nacionalidad: String
+    val nacionalidad: String,
+    val imagen: Int
+
 ){
 }
